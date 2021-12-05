cacdbd07e50a7f675136211409ca4314ef6b68ad
Josiah Hilden <jhilden13@gmail.com>
2021-12-05 01:30:20 -0600

    day3 b attempt 1

--- src/day3.rs
+++ src/day3.rs
@@ -43,7 +43,79 @@ impl DayTask for Day3 {
         Ok((gamma, epsilon))
     }
 
-    fn run_b(_lines: Vec<String>) -> AResult<Self::Out> {
-        todo!()
+    fn run_b(lines: Vec<String>) -> AResult<Self::Out> {
+        let cutoff = lines.len();
+        let len = lines.get(0).context("There was no input")?.len();
+
+        // TODO(josiah) the counts have to change for these on each iteration of the map below, so
+        // the method of only calculating indicies once will not work.
+
+        let counts = lines
+            .iter()
+            .map(|l| l.chars().map(|c| if c == '1' { 1 } else { 0 }))
+            .fold(
+                {
+                    let mut it = Vec::with_capacity(len);
+                    it.resize(len, 0);
+                    it
+                },
+                |mut a, b| {
+                    b.enumerate().for_each(|(idx, it)| a[idx] += it);
+                    a
+                },
+            );
+
+        let (oxygen, co2, _, _) = lines
+            .iter()
+            .map(|l| {
+                (
+                    l,
+                    l.chars().enumerate().find_map(|(idx, c)| {
+                        let v = if counts[idx] > cutoff / 2 { '1' } else { '0' };
+                        if c != v {
+                            Some(idx)
+                        } else {
+                            None
+                        }
+                    }),
+                    l.chars().enumerate().find_map(|(idx, c)| {
+                        let v = if counts[idx] < cutoff / 2 { '1' } else { '0' };
+                        if c != v {
+                            Some(idx)
+                        } else {
+                            None
+                        }
+                    }),
+                )
+            })
+            .fold(
+                (String::new(), String::new(), 0, 0),
+                |(oxy, co2, oxi, coi), (l, oxygen_b, co2_b)| {
+                    let (oxy, oxi) = {
+                        match oxygen_b {
+                            Some(oxygen_b) if oxygen_b > oxi => (l.clone(), oxygen_b),
+                            _ => (oxy, oxi),
+                        }
+                    };
+
+                    let (co2, coi) = {
+                        match co2_b {
+                            Some(co2_b) if co2_b > coi => (l.clone(), co2_b),
+                            _ => (co2, coi),
+                        }
+                    };
+
+                    (oxy, co2, oxi, coi)
+                },
+            );
+
+        fn to_num(val: String) -> u64 {
+            val.chars().into_iter().fold(
+                0,
+                |acc, char| if char == '1' { acc << 1 | 1 } else { acc << 1 },
+            )
+        }
+
+        Ok((to_num(oxygen), to_num(co2)))
     }
 }
